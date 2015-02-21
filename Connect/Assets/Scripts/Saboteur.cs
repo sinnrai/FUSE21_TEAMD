be5bcd479883d3e6eaeac4d2404023cf8bd81141
kasama <sinnrai4@gmail.com>
2015-02-21 14:02:14 +0900

    お邪魔者

---
+++ Connect/Assets/Scripts/Saboteur.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saboteur : MonoBehaviour 
+{
+    void OnMouseDrag() 
+    {
+        Vector3[] FlyDear = {
+            Vector3.up,
+            Vector3.down,
+            Vector3.forward,
+        };
+        float Speed = Random.Range(1, 10);
+        rigidbody.AddForce( FlyDear[Random.Range(0, FlyDear.Length)] * Speed ,ForceMode.VelocityChange);
+    }
+}
