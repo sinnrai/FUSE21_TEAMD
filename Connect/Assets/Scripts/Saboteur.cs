6ada1c67541cd8fd795afad08d7166b429e82773
kasama <sinnrai4@gmail.com>
2015-02-21 14:30:34 +0900

    速度を指定できるようにしたのと回転するように

--- Connect/Assets/Scripts/Saboteur.cs
+++ Connect/Assets/Scripts/Saboteur.cs
@@ -3,14 +3,27 @@ using System.Collections;
 
 public class Saboteur : MonoBehaviour 
 {
+    public float MaxSpeed;
+    public float MinSpeed;
+
+    private bool IsFlying = false;
+
     void OnMouseDrag() 
     {
-        Vector3[] FlyDear = {
-            Vector3.up,
-            Vector3.down,
-            Vector3.forward,
-        };
-        float Speed = Random.Range(1, 10);
-        rigidbody.AddForce( FlyDear[Random.Range(0, FlyDear.Length)] * Speed ,ForceMode.VelocityChange);
+        IsFlying = true;
+        float x = Random.Range(-1, 1);
+        float y = Mathf.Abs(Random.Range(1 / 2, 1));
+        float z = Random.Range(-1, 1);
+        Vector3 FlyDear = new Vector3(x, y, z);
+        float Speed = Random.Range(MinSpeed, MaxSpeed);
+        rigidbody.AddForce(FlyDear * Speed, ForceMode.VelocityChange);
+    }
+
+    void Update()
+    {
+        if (IsFlying)
+        {
+            transform.rotation = Quaternion.Euler( new Vector3(Time.time, Time.time, Time.time));
+        }
     }
 }
