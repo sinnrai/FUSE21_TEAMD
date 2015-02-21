273322b92e26b05ae7a125911a0ae9127895bc46
kasama <sinnrai4@gmail.com>
2015-02-21 13:25:26 +0900

    もろもろ追加

---
+++ Connect/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour 
+{
+	private Vector3 ScreenPoint;
+	private Vector3 offset;
+
+	void OnMouseDown()
+	{
+		this.ScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+		this.offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z));
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 currentScreenPoint = new Vector3 
+                                     (Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z);
+		Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+        transform.position = currentPosition;
+	}
+}
