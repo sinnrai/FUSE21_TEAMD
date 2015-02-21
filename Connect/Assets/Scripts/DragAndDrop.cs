569606677fef1674c4322103714a3692c47ad94b
kasama <sinnrai4@gmail.com>
2015-02-21 13:45:46 +0900

    y -> z

--- Connect/Assets/Scripts/DragAndDrop.cs
+++ Connect/Assets/Scripts/DragAndDrop.cs
@@ -9,13 +9,13 @@ public class DragAndDrop : MonoBehaviour
 	void OnMouseDown()
 	{
 		this.ScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
-		this.offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z));
+        this.offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, ScreenPoint.y, Input.mousePosition.y));
 	}
 
 	void OnMouseDrag()
 	{
 		Vector3 currentScreenPoint = new Vector3 
-                                     (Input.mousePosition.x, Input.mousePosition.y, ScreenPoint.z);
+                                     (Input.mousePosition.x, ScreenPoint.y, Input.mousePosition.y);
 		Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
         transform.position = currentPosition;
 	}
