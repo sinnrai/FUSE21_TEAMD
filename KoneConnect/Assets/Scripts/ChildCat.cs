259b9b23537718b4b20b22867eae9f5acbfb905d
kasama <sinnrai4@gmail.com>
2015-02-25 20:30:10 +0900

    KoneConnectアップ

---
+++ KoneConnect/Assets/Scripts/ChildCat.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildCat : MonoBehaviour {
+
+	iTweenEvent ite ;
+
+	bool isFall ;
+
+	// Use this for initialization
+	void Start () {
+		ite = GetComponent<iTweenEvent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if(!Physics.SphereCast(transform.position + new Vector3( 0.0f, 0.0f, 0.0f ),1.0f,new Vector3( 0.0f, -1.0f, 0.0f ),out hit, 10.0f)){
+			//Destroy( gameObject ) ;
+			ite.Stop() ;
+			isFall = true ;
+
+			Destroy( gameObject, 3.0f ) ;
+		}
+
+		if( isFall ) {
+			transform.position -= new Vector3( 0.0f, 0.8f, 0.0f ) ;
+			transform.Rotate( new Vector3( 0.0f, 20.0f, 0.0f ) ) ;
+		}
+
+	}
+}
---
+++ KoneConnect/Assets/Scripts/TitleController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToGame1 () {
+		Application.LoadLevel ("Stage1");
+	}
+
+	public void ToGame2 () {
+		Application.LoadLevel ("Stage2");
+	}
+
+	public void ToGame3 () {
+		Application.LoadLevel ("Stage3");
+	}
+}
